780ca70f2c2410fa27d585f8f607690e767e726f
Mac <mac@Macde-MacBook-Pro.local>
2020-04-11 13:02:45 +0800

    2th commit(add count)

--- guess.py
+++ guess.py
@@ -2,12 +2,15 @@
 import random
 
 r = random.randint(1, 100)
-
+count = 0
 while True :
+	count +=1 # count = count + 1 
 	number = input('猜數字：')
 	number = int(number)
+
 	if number == r :
 		print('well done!')
+		print('this is the', count , 'th times you guess')
 		break
 	elif number != r :
 		print("wrong!")
@@ -15,4 +18,6 @@ while True :
 			print('it`s too bigger!')
 		else :
 			print('it`s too smaller!')
+
+	print('this is the', count , 'th times you guess')
 
